f66c37a127a5b8d0f3de159ea459874abe4e08a9
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-05-06 03:45:18 +0600

    patch added

--- index.js
+++ index.js
@@ -90,6 +90,19 @@ async function run() {
         res.send(result)
     })
 
+    app.patch('/bookings/:id',async(req,res)=>{
+       const updatedBooking= req.body;
+       const id= req.params.id
+       const filter= {_id: new ObjectId(id)}
+       const updatedDoc={
+          $set:{
+             Status: updatedBooking.Status
+          }
+       }
+       const result= await bookingCollection.updateOne(filter,updatedDoc);
+       res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
